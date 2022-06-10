59ddf116f6b8b97a74bd0e0fd1372c3e572d601e
bakmeon-kim <bmkim@zinnotech.com>
2022-06-10 09:19:52 +0900

    update

--- index.js
+++ index.js
@@ -10,14 +10,26 @@ async function test1(){
     const abs2_path = path.join(process.cwd(),output)
     const abs3_path = path.resolve(input)
     const abs4_path = path.resolve(output)
+    
+    const function1 = function (err, files) {
+      //handling error
+      if (err) {
+          return console.log('Unable to scan directory: ' + err);
+      } 
+      //listing all files using forEach
+      files.forEach(function (file) {
+          // Do whatever you want to do with the file
+          console.log(file); 
+      });
+  }
     console.log('a'+abs1_path) 
-    console.log('b'+fs.readdirSync(abs1_path))
+    fs.readdir(abs1_path, function1);
     console.log('c'+abs2_path)    
-  	console.log('d'+fs.readdirSync(abs2_path))
+    fs.readdir(abs2_path, function1);
     console.log('e'+abs3_path)    
-	  console.log('f'+fs.readdirSync(abs3_path))
+	  fs.readdir(abs3_path, function1);
     console.log('g'+abs4_path)    
-	  console.log('h'+fs.readdirSync(abs4_path))
+	  fs.readdir(abs4_path, function1);
   }catch (error) {
     core.setFailed(error.message);
   }
