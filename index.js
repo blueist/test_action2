fe92ae813cee78d0e5829e506dc33d06c9fae7dc
blueist <stillblueist@gmail.com>
2022-06-16 11:14:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,45 @@ const github = require('@actions/github');
 //const targetProject = "ZF" 
 const targetProject = "ZREQ"
 
+async function getCommitsFromPayload(octokit, payload) {
+    const commits = payload.commits;
+    const owner   = payload.repository.owner.login;
+    const repo    = payload.repository.name;
+
+    const res = await Promise.all(commits.map(commit => octokit.repos.getCommit({
+        owner, repo, ref: commit.id
+    })));
+    return res.map(res => (<any>res).data);
+}
+
+function updatedFiles(commits) {
+    return uniq(commits.reduce(
+        (accum: any[], commit) => accum.concat(
+            commit.files.filter(f => f.status !== 'removed').map(f => f.filename)
+        ),
+        []
+    ));
+}
+
+const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+
 function test1(){
   const context = github.context;
   console.log("context "+JSON.stringify(context))
 
+  const payload = github.context.payload;
+  const ref     = payload.ref;
+  if (!payload.repository) {
+      throw new Error();
+  }
+  const owner   = payload.repository.owner.login;
+  const repo    = payload.repository.name;
+
+  const commits = await getCommitsFromPayload(octokit, payload);
+  const files = updatedFiles(commits);  
+  
+  console.log(files)
 }
+
 
 test1()
