fa6397be082b7eaa04ff9432b0630f1df545a4f3
blueist <stillblueist@gmail.com>
2022-06-20 14:16:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const fs = require('fs').promises;
+const fs = require('fs')
 const path = require('path');
 const github = require('@actions/github');
 
@@ -10,18 +10,23 @@ const targetProject = "ZREQ"
 
 
 function test1(){
-  const context = github.context;
-  console.log("context "+JSON.stringify(context))
+  const input = core.getInput('input')
+  console.log(input)
+  fs.readFile(input, 'utf-8', function(err, data){
+    console.log(data);            // sample.txt의 내용 출력
+  });
+//   const context = github.context;
+//   console.log("context "+JSON.stringify(context))
 
-  const payload = github.context.payload;
-  const ref     = payload.ref;
-  if (!payload.repository) {
-      throw new Error();
-  }
-  const owner   = payload.repository.owner.login;
-  const repo    = payload.repository.name;
+//   const payload = github.context.payload;
+//   const ref     = payload.ref;
+//   if (!payload.repository) {
+//       throw new Error();
+//   }
+//   const owner   = payload.repository.owner.login;
+//   const repo    = payload.repository.name;
 }
 
-const input = core.getInput('input')
-console.log(input)
-//test1()
+// const input = core.getInput('input')
+// console.log(input)
+test1()
